52f0de0baf17be35ab600d152ec29d4237dc9758
ametwali <104875958+ametwali@users.noreply.github.com>
2023-03-26 21:32:44 -0600

    created observable

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { FoodService } from 'src/app/services/food.service';
 import { Food } from 'src/app/shared/models/Food';
 
@@ -22,15 +23,20 @@ export class HomeComponent implements OnInit{
   //and give the Parameter (foodService) a new instance of FoodService, and we don't have to intanstiate it
   // -> new instance: foodService = new FoodService()
   constructor(private foodService:FoodService, activatedRoute:ActivatedRoute) {        //"FoodService" came from food.service.ts
+    let foodsObservable:Observable<Food[]>;
     activatedRoute.params.subscribe((params) =>{                                       //"ActivatedRoute" is for listening to the router-outlet
                                                                                        //listening to the route params. "subscribe" means anytime that the "params" change, call the function inside the "subscribe"
       if(params['searchTerm'])
-        this.foods = this.foodService.getAllFoodsBySearchTerm(params['searchTerm'])    //give me the search result based on the 'searchTerm'
+        foodsObservable = this.foodService.getAllFoodsBySearchTerm(params['searchTerm'])    //give me the search result based on the 'searchTerm'
       else if(params['tag'])
-        this.foods = this.foodService.getAllFoodsByTag(params['tag']);
+      foodsObservable = this.foodService.getAllFoodsByTag(params['tag']);
       else
-        this.foods = foodService.getAll();                                             // Getting all the foods from sample.foods in "data.ts" through "food.service.ts" method getAll()
+      foodsObservable = foodService.getAll();                                             // Getting all the foods from sample.foods in "data.ts" through "food.service.ts" method getAll()
+
+      foodsObservable.subscribe((serverFoods) => {
+        this.foods = serverFoods;
       })
+    })
 
 
   }
